52f411282384df02e3b04f5f25dd5609a3f58ad2
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-09-18 19:57:37 -0300

    Ajeitando identação do código

--- main.c
+++ main.c
@@ -21,38 +21,38 @@ void menu_dieta(void);
 
 // Função principal
 int main(void) {
-    setlocale(LC_CTYPE , "");
-    menu_principal();
-    menu_cliente();
-    menu_dieta();
-    return 0;
+  setlocale(LC_CTYPE , "");
+  menu_principal();
+  menu_cliente();
+  menu_dieta();
+  return 0;
 }
 
 
 void menu_principal(void) { // Feito por Luís e Diêgo
-    printf("\n");
-    printf("//////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                        ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                ///\n");
-    wprintf(L"///                 Centro de Ensino Superior do Seridó                    ///\n");
-    wprintf(L"///               Departamento de Computação e Tecnologia                  ///\n");
-    wprintf(L"///                    Sistemas de Informação - BSI                        ///\n");
-    wprintf(L"///                  Disciplina DCT1106 -- Programação                     ///\n");
-    printf("///               Projeto Sistema de Planejamento De Dietas                ///\n");
-    wprintf(L"///              Developed by Diêgo Axel and Luís Henrique                 ///\n");
-    printf("///                    Data do projeto : 18/09/2024                        ///\n");
-    printf("///                                                                        ///\n");
-    printf("//////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                        ///\n");
-    printf("///            SIG Diet-Plan (Sistema de planejamento de dietas)           ///\n");
-    printf("///                                                                        ///\n");
-    wprintf(L"///                         [1] Módulo Cliente                             ///\n");
-    wprintf(L"///                         [2] Módulo Dietas                              ///\n"); 
-    wprintf(L"///                         [3] Módulo Agendamento                         ///\n"); 
-    wprintf(L"///                         [4] Módulo Relatório                           ///\n"); 
-    wprintf(L"///                         [5] Módulo Informações                         ///\n"); 
-    printf("///                                                                        ///\n");
-    printf("//////////////////////////////////////////////////////////////////////////////\n");
+  printf("\n");
+  printf("//////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                        ///\n");
+  printf("///             Universidade Federal do Rio Grande do Norte                ///\n");
+  wprintf(L"///                 Centro de Ensino Superior do Seridó                    ///\n");
+  wprintf(L"///               Departamento de Computação e Tecnologia                  ///\n");
+  wprintf(L"///                    Sistemas de Informação - BSI                        ///\n");
+  wprintf(L"///                  Disciplina DCT1106 -- Programação                     ///\n");
+  printf("///               Projeto Sistema de Planejamento De Dietas                ///\n");
+  wprintf(L"///              Developed by Diêgo Axel and Luís Henrique                 ///\n");
+  printf("///                    Data do projeto : 18/09/2024                        ///\n");
+  printf("///                                                                        ///\n");
+  printf("//////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                        ///\n");
+  printf("///            SIG Diet-Plan (Sistema de planejamento de dietas)           ///\n");
+  printf("///                                                                        ///\n");
+  wprintf(L"///                         [1] Módulo Cliente                             ///\n");
+  wprintf(L"///                         [2] Módulo Dietas                              ///\n"); 
+  wprintf(L"///                         [3] Módulo Agendamento                         ///\n"); 
+  wprintf(L"///                         [4] Módulo Relatório                           ///\n"); 
+  wprintf(L"///                         [5] Módulo Informações                         ///\n"); 
+  printf("///                                                                        ///\n");
+  printf("//////////////////////////////////////////////////////////////////////////////\n");
 } 
 
 
