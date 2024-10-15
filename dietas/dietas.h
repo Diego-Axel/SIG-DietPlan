5d0a6e114f3c60456d8fab6df714dec290c8f393
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-10-15 18:44:48 -0300

    arquivo das assinaturas das funções de dietas

---
+++ dietas/dietas.h
@@ -0,0 +1,13 @@
+// Assinatura das funções
+
+#ifndef DIETAS
+#define DIETAS
+
+char menu_dieta(void);
+void avaliacao(void);
+void dieta_para_hipertrofia(void);
+void dieta_para_perda_de_peso(void);
+void reavaliacao(void);
+void nova_dieta(void);
+
+#endif
