54cc3cb14c6843807b481c008b9313b0ecc385e1
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-10-15 18:12:15 -0300

    Criei o arquivo que contém as assinaturas das funções de clientes

---
+++ clientes/clientes.h
@@ -0,0 +1,11 @@
+// Assinatura das funções
+#ifndef CLIENTE
+#define CLIENTE
+
+char menu_cliente(void);
+void cadastrar_cliente(void);
+void exibir_cliente(void);
+void alterar_cliente(void);
+void excluir_cliente(void);
+
+#endif
