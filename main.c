9a3c6298a6aada702de0fb4b48fee813be393c2d
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-10-15 22:40:07 -0300

    importando a biblioteca de interfaces do menu principal

--- main.c
+++ main.c
@@ -16,8 +16,8 @@
 #include <ctype.h>
 
 
-// Assinatura das funções
-char menu_principal(void);
+// Assinatura das funções do Menu Principal (interfaces)
+#include "interfacePrincipal/interfaces.h"
 
 // Assinatura do cliente
 #include "clientes/clientes.h"
@@ -42,10 +42,6 @@ void excluir_prof(void);
 char menu_relatorio(void);
 void relatorio_clientes(void);
 void relatorio_profissional(void);
-
-// Assinatura informações
-void menu_info(void);
-void menu_dev(void);
 
 
 // Programa Principal
@@ -151,48 +147,6 @@ int main(void) {
   } while (op_principal != '0');
   return 0;
 }
-
-
-char menu_principal(void) {
-
-  char op_principal;
-
-  system("clear || cls"); // se for Linux use 'clear' se for Windows use 'cls'
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///             SIG Diet-Plan Sistema de Planejamento de Dietas            ///\n");
-  wprintf(L"\t///                              Bem vindo(a)                              ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                         [1] Módulo Cliente                             ///\n");
-  wprintf(L"\t///                         [2] Módulo Dietas                              ///\n"); 
-  wprintf(L"\t///                         [3] Módulo Agendamento                         ///\n");
-  wprintf(L"\t///                         [4] Módulo Profissional                        ///\n"); 
-  wprintf(L"\t///                         [5] Módulo Relatório                           ///\n"); 
-  wprintf(L"\t///                         [6] Módulo Informações                         ///\n");
-  wprintf(L"\t///                         [7] Módulo Desenvolvedores                     ///\n");
-  wprintf(L"\t///                         [0] Encerrar Programa                          ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\n"); 
-  wprintf(L"\t//// Escolha uma opção: ");
-  scanf("%c", &op_principal);
-  getchar();
-  return op_principal;
-} 
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 char menu_agendamento(void) {
@@ -640,74 +594,4 @@ void relatorio_profissional(void) {
   wprintf(L"\ttecle <ENTER> para retornar... ");
   getchar();
   
-}
-
-
-void menu_info(void) {
-  system("clear || cls"); // se for Linux use 'clear' se for Windows use 'cls'
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                            Módulo informações                          ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///               Universidade Federal do Rio Grande do Norte              ///\n");
-  wprintf(L"\t///                   Centro de Ensino Superior do Seridó                  ///\n");
-  wprintf(L"\t///                 Departamento de Computação e Tecnologia                ///\n");
-  wprintf(L"\t///                      Sistemas de Informação - BSI                      ///\n");
-  wprintf(L"\t///                    Disciplina DCT1106 -- Programação                   ///\n");
-  wprintf(L"\t///                 Projeto Sistema de Planejamento De Dietas              ///\n");
-  wprintf(L"\t///                Developed by Diêgo Axel and Luís Henrique               ///\n");
-  wprintf(L"\t///                      Data do projeto : 18/09/2024                      ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                             Sobre o programa:                          ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t/// SIG-DietPlan, é um software próprio para você que é um profissional na ///\n"); 
-  wprintf(L"\t/// área da saúde, especialmente nutricionista, que quer ter um controle   ///\n");
-  wprintf(L"\t/// preciso e bastante eficiente em sua clínica. SIG-DietPlan permite você ///\n"); 
-  wprintf(L"\t/// além de ter todo o controle de gestão sobre o seu negócio, faz com que ///\n");
-  wprintf(L"\t/// você(profissional) possa cadastrar e acompanhar uma dieta completa dos ///\n");  
-  wprintf(L"\t/// seus clientes. SIG-DietPlan, a solução para os seus problemas.         ///\n"); 
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///         Acesse esse Link para conferir o repositório no GitHub:        ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///              https://github.com/Diego-Axel/SIG-DietPlan.git            ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                              MIT Licence                               ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\n"); 
-  wprintf(L"\tTecle <ENTER> para retornar...");
-  getchar();
-}
-
-
-void menu_dev(void) {
-  system("clear || cls"); // se for Linux use 'clear' se for Windows use 'cls'
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                          Módulo desenvolvedores                        ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                             Desenvolvedores:                           ///\n");
-  wprintf(L"\t///      Luís Henrique                                      Diêgo Axel     ///\n");
-  wprintf(L"\t///------------------------------------------------------------------------///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                                 GitHub:                                ///\n");
-  wprintf(L"\t///         Luis27h                                         Diego-Axel     ///\n");
-  wprintf(L"\t///------------------------------------------------------------------------///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t///                            Entre Em Contato:                           ///\n");
-  wprintf(L"\t///  luis27marciano @gmail.com                    diegoaxelbsr@gmail.com   ///\n");
-  wprintf(L"\t///       (84) 99627-4880                             (84) 99977-4459      ///\n");
-  wprintf(L"\t///                                                                        ///\n");
-  wprintf(L"\t//////////////////////////////////////////////////////////////////////////////\n");
-  wprintf(L"\n"); 
-  wprintf(L"\tTecle <ENTER> para retornar...");
-  getchar();
 }
