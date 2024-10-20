d3b1904057a22e5b2b2610524c5a61ed303ca803
Luis_H <luis27marciano@gmail.com>
2024-10-20 17:22:40 -0300

    Módulo profissional
    Módulo profissional criado e informações inseridas.

---
+++ profissional/profissional.h
@@ -0,0 +1,12 @@
+// Assinatura das funções
+
+#ifndef PROFISSIONAL
+#define PROFISSIONAL
+
+char menu_profissional(void);
+void cadastrar_prof(void);
+void exibir_prof(void);
+void recadastrar_prof(void);
+void excluir_prof(void);
+
+#endif
