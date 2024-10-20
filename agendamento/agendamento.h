7c3a31d3ce2e832e720344c871a1df3e00813947
Luis_H <luis27marciano@gmail.com>
2024-10-20 17:02:31 -0300

    Modulação agendamento
    Criado e inserido as informações necessárias na pasta de agendamento.

---
+++ agendamento/agendamento.h
@@ -0,0 +1,11 @@
+// Assinatura das funções
+
+#ifndef AGENDAMENTO
+#define AGENDAMENTO
+
+char menu_agendamento(void);
+void agendar(void);
+void exibir_agendamento(void);
+void excluir_agendamento(void);
+
+#endif
