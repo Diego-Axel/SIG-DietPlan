58543ba77e3a5978000cd1ab0f567457171cb04c
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2024-11-02 20:01:14 -0300

    Cria função "eLetra"
    função para validar se o caractere inserido é uma letra entre ('A' e 'Z' ou 'a' e 'z')

--- validadores/validadores.c
+++ validadores/validadores.c
@@ -8,12 +8,26 @@
 // Funções 
 
 // Função para verificar se um caractere é um digito
+// Adaptado do Projeto Lingua Solta
 
 int eDigito(char c) {
-    if (c <= '9' && c >= '0') {        // retorna 1 caso o caractere inserido esteja entre 0 e 9 
+    if (c <= '9' && c >= '0') {        // retorna 1 caso o caractere recebido esteja entre 0 e 9 
         return 1;
     } else {
         return 0;
     }
 }
+ 
 
+// Função para verificar se um caractere é uma letra
+// Adaptado do Projeto Lingua Solta
+
+int eLetra(char c) {
+  if (c >= 'A' && c <= 'Z') {          // retorna 1 caso o caractere recebido seja uma letra entre ('A' e 'Z' ou 'a' e 'z')
+    return 1;
+  } else if (c >= 'a' && c <= 'z') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
