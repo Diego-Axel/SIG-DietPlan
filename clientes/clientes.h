2824f02bbaf65775df01e3fb6fd022fbc28db621
Luis_H <luis27marciano@gmail.com>
2024-12-08 15:24:36 -0300

    Struct do cliente criado.
    Iniciado o struct do módulo cliente.

--- clientes/clientes.h
+++ clientes/clientes.h
@@ -3,6 +3,15 @@
 #ifndef CLIENTES
 #define CLIENTES
 
+typedef struct clientes Clientes;
+
+struct clientes {
+    char nome[40];
+    char email[30];
+    char telefone[16];
+    char cpf[13];
+};
+
 char menu_cliente(void);
 void cadastrar_cliente(void);
 void exibir_cliente(void);
