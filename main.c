63dde94041b2ef4a3828739560c91370db54cc1e
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2024-12-17 22:02:56 -0300

    Altera estrutura no main.c para switch case

--- main.c
+++ main.c
@@ -38,102 +38,26 @@
 // Programa Principal
 int main(void) {
 
-  // Variáveis para as opções
-  char op_principal;
-  char op_cliente;
-  char op_dietas;
-  char op_agendar;
-  char op_profissional;
-  char op_relatorio;
-  
+  char opcao;
 
-  do { 
-    op_principal = menu_principal();
-    if (op_principal == '1') {
-      do {
-        op_cliente = menu_cliente();
-        if (op_cliente == '1') {
-          cadastrar_cliente();
+  do {
+        opcao = menu_principal();
+
+        switch(opcao) {
+            case '1': menu_dieta(); 
+                      break;
+            case '2': menu_cliente();
+                      break;
+            case '3': menu_profissional();
+                      break;
+            case '4': menu_relatorio();
+                      break;
+            case '5': menu_info();
+                      break;
+            case '6': menu_dev();
+                      break;
         }
-        else if (op_cliente == '2') {
-          pesquisar_cliente();
-        }
-        else if (op_cliente == '3') {
-          atualiza_cliente();
-        }
-        else if (op_cliente == '4') {
-          excluir_cliente();
-        }  
-      } while (op_cliente != '0');    
-    }
-    else if (op_principal == '2') {
-      do {
-        op_dietas = menu_dieta();
-        if (op_dietas == '1') {
-          avaliacao();
-        }
-        else if (op_dietas == '2') {
-          dieta_para_hipertrofia();
-        }
-        else if (op_dietas == '3') {
-          dieta_para_perda_de_peso();
-        }
-        else if (op_dietas == '4') {
-          reavaliacao();
-        }
-        else if (op_dietas == '5') {
-          nova_dieta();
-        }
-      } while (op_dietas != '0'); 
-    }
-    else if (op_principal == '3') {
-      do {
-        op_agendar = menu_agendamento();
-        if (op_agendar == '1') {
-          agendar();
-        }
-        else if (op_agendar == '2')  {
-          exibir_agendamento();
-        }
-        else if (op_agendar == '3') {
-          excluir_agendamento();
-        }
-      } while (op_agendar != '0');
-    }
-    else if (op_principal == '4') {
-      do {
-        op_profissional = menu_profissional();
-        if (op_profissional == '1') {
-          cadastrar_prof();
-        }
-        else if (op_profissional == '2') {
-          pesquisar_prof();
-        }
-        else if (op_profissional == '3') {
-          atualiza_prof();
-        }
-        else if (op_profissional == '4') {
-          excluir_prof();
-        }
-      } while (op_profissional != '0');
-    }
-    else if (op_principal == '5') {
-      do {
-        op_relatorio = menu_relatorio();
-        if (op_relatorio == '1') {
-          relatorio_clientes();
-        }
-        else if (op_relatorio == '2') {
-          relatorio_profissional();
-        }
-      } while (op_relatorio != '0');   
-    }
-    else if (op_principal == '6') {
-      menu_info();
-    }
-    else if (op_principal == '7') {
-      menu_dev();
-    }
-  } while (op_principal != '0');
+  } while(opcao != '0');
+
   return 0;
 }
